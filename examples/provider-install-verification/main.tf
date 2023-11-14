debae6729c50e70d561b8564e09d4312f9c4ace3
Tomer Admon <tomer.a@qualisystems.com>
2023-11-14 17:45:20 +0200

    Update main.tf

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -10,6 +10,6 @@ provider "torque" {}
 
 resource "torque_introspection_resource" "example" {
     display_name = "My Resource"
-    image = "https://cdn-icons-png.flaticon.com/512/882/882730.png"
+    image = "https://portal.qtorque.io/static/media/networking.dc1b7fb73182de0136d059a1eb00af4f.svg"
     introspection_data = {size = "large", mode = "party"}
 }
