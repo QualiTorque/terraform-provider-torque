097e6c1d9624a3849d5bf16ac1a89e06d9b49044
Tomer Admon <Tomer.a@qualisystems.com>
2024-02-11 21:33:52 +0200

    .

--- examples/user-group/variables.tf
+++ examples/user-group/variables.tf
@@ -1,41 +1,43 @@
 variable "torque_space" {
-  type    = string
+  type        = string
+  description = "Torque space used for authenticating into Torque"
 }
 
 variable "torque_token" {
-  type    = string
-  sensitive = true
+  type        = string
+  sensitive   = true
+  description = "Torque token used to authenticate to the Torque API. Supports both short and long tokens"
 }
 
 variable "group_name" {
-  type    = string
-  default = "my-test-group"
+  type        = string
+  default     = "my-test-group"
+  description = "The new Torque group name to be created"
 }
 
 
 variable "group_description" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "The new Torque group description to be used"
 }
 
 variable "group_idp_identifier" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "The Identity Provider ID to be used with the new Torque group"
 }
 
 variable "group_account_role" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
+  description = "Choose this to set the group as an account level group. Possible values: \"Admin\" or \"Member\""
 }
 
 variable "group_users" {
-  type    = list(string)
-  default = []
-}
-
-variable "group_users" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
+  description = "List of user emails to assign to the group"
 }
 
 variable "group_space_roles" {
@@ -43,5 +45,6 @@ variable "group_space_roles" {
     space_name = string
     space_role = string
   }))
-  default = []
+  default     = []
+  description = "Use this to set the group scope for specific spaces and the role of the group within the spaces."
 }
