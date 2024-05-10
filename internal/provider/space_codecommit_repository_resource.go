daf699b64018ac93f7bb88cdd6dfc8dd6860c434
amiros89 <amir.rashko@gmail.com>
2024-05-10 10:02:22 +0300

    force recreate code commit repo upon update

--- internal/provider/space_codecommit_repository_resource.go
+++ internal/provider/space_codecommit_repository_resource.go
@@ -7,6 +7,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/qualitorque/terraform-provider-torque/client"
@@ -49,38 +51,65 @@ func (r *TorqueSpaceCodeCommitRepositoryResource) Schema(ctx context.Context, re
 			"space_name": schema.StringAttribute{
 				MarkdownDescription: "Existing Torque Space name",
 				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"repository_url": schema.StringAttribute{
 				Description: "Repository URL. For example: https://git-codecommit.eu-west-1.amazonaws.com/v1/repos/repo",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"role_arn": schema.StringAttribute{
 				Description: "AWS Role ARN for Torque to use which has permissions to connect to CodeCommit",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"aws_region": schema.StringAttribute{
 				Description: "AWS Region that hosts the CodeCommit Repository, i.e eu-west-1",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"external_id": schema.StringAttribute{
 				Description: "External ID used in the IAM role trust policy.",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"git_username": schema.StringAttribute{
 				Description: "Git Username",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"git_password": schema.StringAttribute{
 				Description: "Git Password",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"branch": schema.StringAttribute{
 				Description: "Repository branch to use for blueprints and automation assets",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"repository_name": schema.StringAttribute{
 				Description: "The name of the CodeCommit repository to onboard",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 		},
 	}
@@ -151,25 +180,25 @@ func (r *TorqueSpaceCodeCommitRepositoryResource) Read(ctx context.Context, req 
 }
 
 func (r *TorqueSpaceCodeCommitRepositoryResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data TorqueSpaceCodeCommitRepositoryResourceModel
+	// var data TorqueSpaceCodeCommitRepositoryResourceModel
 
-	// Read Terraform plan data into the model
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	// // Read Terraform plan data into the model
+	// resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
 
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
-	//     return
+	// if resp.Diagnostics.HasError() {
+	// 	return
 	// }
 
-	// Save updated data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	// // If applicable, this is a great opportunity to initialize any necessary
+	// // provider client data and make a call using it.
+	// // httpResp, err := r.client.Do(httpReq)
+	// // if err != nil {
+	// //     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
+	// //     return
+	// // }
+
+	// // Save updated data into Terraform state
+	// resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *TorqueSpaceCodeCommitRepositoryResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
