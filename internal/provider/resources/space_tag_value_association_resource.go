2c2f6410a5c8d4d5287a690e2c9ea4879042a999
Amir Rashkovsky <62113999+amiros89@users.noreply.github.com>
2024-12-03 14:52:17 +0200

    fix modify blueprint/space tag behavior  (#86)
    * add plan modifiers to require replace if blueprint and tag names were changed
    
    * update env data source test

--- internal/provider/resources/space_tag_value_association_resource.go
+++ internal/provider/resources/space_tag_value_association_resource.go
@@ -8,6 +8,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/qualitorque/terraform-provider-torque/client"
@@ -44,10 +46,16 @@ func (r *TorqueTagSpaceValueAssociationResource) Schema(ctx context.Context, req
 			"space_name": schema.StringAttribute{
 				MarkdownDescription: "Existing Torque Space name",
 				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"tag_name": schema.StringAttribute{
 				Description: "Tag name configured in the account",
 				Required:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"tag_value": schema.StringAttribute{
 				Description: "The tag value to be set for the space",
