ef7862aa98f71aa6026a9d01d660fa6426406180
amiros89 <amir.rashko@gmail.com>
2024-12-30 10:19:36 +0200

    implement update blueprint policies func

--- client/blueprint.go
+++ client/blueprint.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -35,4 +37,33 @@ func (c *Client) GetBlueprint(space_name string, name string) (*Blueprint, error
 	}
 
 	return nil, fmt.Errorf("blueprint %s not found", name)
+}
+
+func (c *Client) SetBlueprintPolicies(space_name string, repository_name string, name string, max_duration string, default_duration string, default_extend string, max_active_environments int32, always_on bool) error {
+	data := Policies{
+		MaxDuration:           max_duration,
+		DefaultDuration:       default_duration,
+		DefaultExtend:         default_extend,
+		MaxActiveEnvironments: max_active_environments,
+		AlwaysOn:              always_on,
+	}
+	payload, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("impossible to marshall agent association: %s", err)
+	}
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%sapi/spaces/%s/repositories/%s/blueprints/%s/policies", c.HostURL, space_name, repository_name, name), bytes.NewReader(payload))
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	_, err = c.doRequest(req, &c.Token)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
