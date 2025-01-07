b500fceaf0e9a3f787a124cff6e8965fd02ed908
amiros89 <amir.rashko@gmail.com>
2025-01-07 16:31:28 +0200

    test adjustments

--- internal/provider/tests/s3_object_content_input_source_test.go
+++ internal/provider/tests/s3_object_content_input_source_test.go
@@ -17,12 +17,14 @@ func TestS3ObjectContentInputSource(t *testing.T) {
 	const (
 		input_name            = "s3"
 		description           = "s3 input test"
+		space                 = "TorqueTerraformProvider"
 		credential_name       = "TerraformCreds"
 		new_credential_name   = "TerraformCreds2"
 		pattern               = "pattern"
 		new_pattern           = "new_pattern"
 		json_path             = "/"
 		new_json_path         = "/new/path"
+		display_json_path     = "/json/display/path"
 		new_display_json_path = "/new/display/json/path"
 		new_key               = "new_key"
 		key                   = "key"
@@ -41,31 +43,31 @@ func TestS3ObjectContentInputSource(t *testing.T) {
 			{
 				Config: providerConfig + fmt.Sprintf(`
 				resource "torque_s3_object_content_input_source" "s3_bucket" {
-					name                       = "%s"
-					description                = "%s"
-					bucket_name                = "%s"
-					credential_name            = "%s"
-					filter_pattern_overridable = false
-					filter_pattern             = "pattern"
-					json_path                = "path"
-					json_path_overridable    = false
-					object_key = "key"
-					display_json_path = "path"
+					name                          = "%s"
+					description                   = "%s"
+					bucket_name                   = "%s"
+					credential_name               = "%s"
+					filter_pattern_overridable    = false
+					filter_pattern                = "%s"
+					json_path                     = "%s"
+					json_path_overridable         = false
+					object_key                    = "%s"
+					display_json_path             = "%s"
 					display_json_path_overridable = true
 				}
-				`, unique_name, description, unique_bucket, credential_name),
+				`, unique_name, description, unique_bucket, credential_name, pattern, json_path, key, display_json_path),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "name", unique_name),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "bucket_name", unique_bucket),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "credential_name", credential_name),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "all_spaces", "true"),
-					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "filter_pattern", "pattern"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "filter_pattern", pattern),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "filter_pattern_overridable", "false"),
-					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "object_key", "key"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "object_key", key),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "object_key_overridable", "false"),
-					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "json_path", "path"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "json_path", json_path),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "json_path_overridable", "false"),
-					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "display_json_path", "path"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "display_json_path", display_json_path),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "display_json_path_overridable", "true"),
 				),
 			},
@@ -84,12 +86,13 @@ func TestS3ObjectContentInputSource(t *testing.T) {
 					object_key                    = "%s"
 					display_json_path             = "%s"
 					display_json_path_overridable = false
-					specific_spaces               = ["TorqueTerraformProvider"]
+					specific_spaces               = ["%s"]
 				}
-				`, unique_name, description, unique_bucket, new_credential_name, new_pattern, new_json_path, new_key, new_display_json_path),
+				`, unique_name, description, unique_bucket, new_credential_name, new_pattern, new_json_path, new_key, new_display_json_path, space),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "name", unique_name+"_new"),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "bucket_name", unique_bucket+"_new"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "description", description+"_new"),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "credential_name", new_credential_name),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "filter_pattern", new_pattern),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "filter_pattern_overridable", "true"),
@@ -98,7 +101,7 @@ func TestS3ObjectContentInputSource(t *testing.T) {
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "display_json_path", new_display_json_path),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "display_json_path_overridable", "false"),
 					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "all_spaces", "false"),
-					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "specific_spaces.0", "TorqueTerraformProvider"),
+					resource.TestCheckResourceAttr("torque_s3_object_content_input_source.s3_bucket", "specific_spaces.0", space),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
