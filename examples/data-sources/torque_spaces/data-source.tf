a9fee7559eb4d5e583d4b0ba4696a6d45a80b088
Amir Rashkovsky <62113999+amiros89@users.noreply.github.com>
2025-01-13 15:59:39 +0200

    Spaces data source (#108)
    * new spaces data source
    
    * remove comment
    
    * lint test

---
+++ examples/data-sources/torque_spaces/data-source.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    torque = {
+      source = "qualitorque/torque"
+    }
+  }
+}
+
+provider "torque" {
+  host  = "https://portal.qtorque.io/"
+  space = "space"
+  token = "111111111111"
+}
+
+data "torque_spaces" "torque_spaces" {
+}
---
+++ internal/provider/tests/spaces_data_source_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestSpacesDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+				data "torque_spaces" "torque_spaces" {
+				}
+				`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces").AtSliceIndex(0).AtMapKey("name"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces").AtSliceIndex(0).AtMapKey("icon"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces").AtSliceIndex(0).AtMapKey("color"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces").AtSliceIndex(0).AtMapKey("num_of_users"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"data.torque_spaces.torque_spaces",
+						tfjsonpath.New("spaces").AtSliceIndex(0).AtMapKey("num_of_groups"),
+						knownvalue.NotNull(),
+					),
+				},
+			},
+		},
+	})
+}
